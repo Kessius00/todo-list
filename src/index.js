74f985d83c80aceeebf2e9c2f81e87a2902d160a
kessius xubuntu <kes.olling00@gmail.com>
2023-11-08 01:57:07 +0100

    make layout for the page and add styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import "./main.css";
 import "./reset.css";
+import "./main.css";
 import ToDo from "./todos";
 
-const content = document.querySelector("#content");
-
+// const content = document.querySelector("#content");
+const dededede = new ToDo("clear room", "ded","De","d","dede");
+dededede.displayDetails()
--- src/todos.js
+++ src/todos.js
@@ -1,9 +1,36 @@
 
-export class ToDo{
+
+export default class ToDo{
     constructor(title, description, dueDate, priority,checklist, notes){
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.timeOfCreation = new Date();
     }
+
+    displayDetails(){
+        console.log(`Title: ${this.title}, description: ${this.description}, due date: ${this.dueDate}, priority: ${this.priority}, made on ${this.timeOfCreation}`);
+        return this.title,this.description,this.dueDate,this.priority
+    }
+
+    deadline(){
+        let tool = (this.dueDate-this.description);
+        return tool
+    }
+    
 }
+
+
+let thing = new ToDo("Wdw",new Date("2/2/2030"),new Date("2/1/2030"));
+
+console.log("deadlinecheck: "+thing.deadline()/(1000*24*60*60));
+
+
+
+
+
+
+
+
+
