3e9f0dbac6759c3de634649b2ecd5a79ba948a49
kessius xubuntu <kes.olling00@gmail.com>
2023-11-08 23:54:39 +0100

    Add sass-loader and cleaner.sass file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import "./reset.css";
-import "./main.css";
+// import "./main.css";
+import "./cleaner.sass";
 import ToDo from "./todos";
 
 // const content = document.querySelector("#content");
 const dededede = new ToDo("clear room", "ded","De","d","dede");
 dededede.displayDetails()
+
+const todoList = document.querySelector("#todo-list");
+
+const newListItem = document.createElement("li");
+newListItem.classList.add("todo-item");
+newListItem.id = "todo-item";
+newListItem.textContent = "textext";
+
+todoList.appendChild(newListItem);
--- webpack.prod.js
+++ webpack.prod.js
@@ -14,7 +14,19 @@ module.exports = merge(common, {
             {
                 test: /\.css$/i,
                 use: [MiniCssExtractPlugin.loader, "css-loader"]
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  MiniCssExtractPlugin.loader,
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+              },
+
         ]
     },
     optimization: {
