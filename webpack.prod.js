99d8d5b104c8b210ed4ea075a585622834f50418
kessius xubuntu <kes.olling00@gmail.com>
2024-01-02 11:32:58 +0100

    production is possible, making dist folder, CSS minimizer doesn't work

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,15 @@
 const path = require("path");
-const common = require("./webpack.common");
+const common = require("./webpack.common.js");
 const {merge} = require("webpack-merge");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {MiniCssExtractPlugin} = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// for minimizing, but it doesn't work..
+// const TerserPlugin = require("terser-webpack-plugin");
+// const OptimizeCssAssetsPlugin = require("css-minimizer-webpack-plugin");
+// const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
@@ -26,26 +30,36 @@ module.exports = merge(common, {
                   "sass-loader",
                 ],
               },
-
-        ]
+        ],
     },
     optimization: {
         minimizer: [
-            new OptimizeCssAssetsPlugin(), new TerserPlugin()
-        ]
+            // new OptimizeCssAssetsPlugin(),
+            // new CssMinimizerPlugin(),
+            // test dit opnieuw, wat gaat er fout? ^^^
+            
+            // new TerserPlugin(),
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers
+            '...',
+            // new CssMinimizerPlugin(),
+            
+        ],
     },
     output: {
         filename: "main.[contenthash].js",
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "dist"),
     },
-    plugins: [new MiniCssExtractPlugin({ filename: "[name].[contenthash].css"}), new CleanWebpackPlugin(), new HtmlWebpackPlugin({
+    plugins: [new MiniCssExtractPlugin({ filename: "[name].[contenthash].css"}
+    ), new CleanWebpackPlugin(), new HtmlWebpackPlugin({
         template: "./src/template.html",
         minify:{
-          removeAttributeQuotes: true,  plugins: [new HtmlWebpackPlugin({
-            template: "./src/template.html"
-          })],
+          // This option removes attribute quotes, which can slightly reduce the size of the HTML file but can also break web pages that depend on attribute quotes for semantic purposes or to handle special characters. It's generally better to avoid using this option and rely on a specific minification tool for HTML files.
+
+          // removeAttributeQuotes: true,  plugins: [new HtmlWebpackPlugin({
+          //   template: "./src/template.html"
+          // })],
           collapseWhitespace: true,
-          removeComments: true
+          removeComments: true,
         }
       })],
 
